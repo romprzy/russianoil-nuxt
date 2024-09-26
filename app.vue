3e9e7ad88942d645b2c3d7c2d20ef7d62037cef6
Roman Przybylski <romprzy81@gmail.com>
2024-09-26 16:04:48 +0200

    Preparation for deployment (GitHub pages and DigitalOcean).

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
 <template>
   <NuxtLayout />
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Russian Oil',
+  meta: [
+    {
+      name: 'description',
+      content: 'Source of information about Russian oil industry and more.',
+    },
+  ],
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config'
 
+const owner = process.env.GITHUB_REPOSITORY_OWNER
+const repository = process.env.GITHUB_REPOSITORY
+const baseURL = repository?.replace(owner, '') || '' + '/'
+
 export default defineNuxtConfig({
+  app: {
+    baseURL,
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   build: {
